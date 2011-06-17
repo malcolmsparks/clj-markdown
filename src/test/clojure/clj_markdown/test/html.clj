c5a51e7f141058789af659d964f39b1e0fa99f5e
Malcolm Sparks <malcolm@congreve.com>
2011-06-17 19:24:19 +0100

    Tidied HTML module.

--- src/test/clojure/clj_markdown/test/html.clj
+++ src/test/clojure/clj_markdown/test/html.clj
@@ -2,26 +2,55 @@
   (:use clojure.test clj-markdown.core clojure.contrib.pprint)
   (:require
    [clojure.java.io :as io]
-   [clojure.zip :as zip]))
-
-(letfn [
-        (to-html-heading [k] (keyword (str \h (last (name k)))))
-        (emit-html-for-block [block]
-                             (let [root (zip/vector-zip block)
-                                   marker (zip/node (zip/next root))]
-                               (zip/root
-                                (cond
-                                 (contains? (apply hash-set (map #(keyword (str 'clj-markdown.core) %) (map #(str "heading" %) (range 1 (inc 6))))) marker)
-                                 (zip/replace (zip/next root) (to-html-heading marker))
-
-                                 (= marker :clj-markdown.core/xml)
-                                 (zip/edit root second)
-
-                                 :otherwise root))))]
-
-  (pprint (take 3 (map emit-html-for-block
-                       (markdown
-                        (.getResourceAsStream (class System) "/markdown-tests/Markdown Documentation - Basics.text"))))))
+   [clojure.zip :as zip]
+   [clojure.contrib.prxml :as prxml]))
 
 
+(defn emit-html-for-block
+  [block]
+  (letfn [(to-html-heading [k] (keyword (str \h (last (name k)))))]
+    (let [root (zip/vector-zip block)
+          marker (zip/node (zip/next root))
+          head (zip/next root)]
+      (zip/root
+       (cond
+        (contains? (apply hash-set (map #(keyword (str 'clj-markdown.core) %)
+                                        (map #(str "heading" %)
+                                             (range 1 (inc 6)))))
+                   marker)
+        (zip/replace head (to-html-heading marker))
 
+        (= marker :clj-markdown.core/xml)
+        (zip/edit root second)
+
+        (= marker :clj-markdown.core/para)
+        (->
+         head
+         (zip/replace :p)
+         (zip/next)
+         (zip/edit #(reduce str (interpose " " %))))
+
+        (= marker :clj-markdown.core/code-block)
+        (->
+         head
+         (zip/replace :pre)
+         (zip/next)
+         (zip/edit #(reduce str (interpose " " %))))
+
+        :otherwise root)))))
+
+(defn emit-html [blocks]
+  (vec (cons :div (map emit-html-for-block blocks))))
+
+(defn print-html [blocks]
+  (binding [prxml/*prxml-indent* 4]
+    (prxml/prxml (emit-html blocks))))
+
+(pprint
+ (emit-html
+  (markdown
+   (.getResourceAsStream (class System) "/markdown-tests/Markdown Documentation - Basics.text"))))
+
+(print-html
+ (markdown
+  (.getResourceAsStream (class System) "/markdown-tests/Markdown Documentation - Basics.text")))
